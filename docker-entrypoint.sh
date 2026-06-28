85fcd6a3f90c3d0afd0229c153eed42b1aaae7bf
Nikhil Wagh <252108817+NikhilWagh1018@users.noreply.github.com>
2026-06-28 15:11:00 +0530

    fix: rewrite docker-entrypoint.sh with correct PORT variable in virtualhost config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 PORT="${PORT:-80}"
 echo "Starting Apache on port $PORT"
 sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/:80>/:>/" /etc/apache2/sites-enabled/000-default.conf
+sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
 exec apache2ctl -D FOREGROUND
