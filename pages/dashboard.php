8931c82cabb45f55f12f4cfd382e18d3f2fe3c2d
Nikhil Wagh <252108817+NikhilWagh1018@users.noreply.github.com>
2026-08-10 22:59:29 +0530

    Remove Road Setup nav item from surveyor sidebar

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -42,10 +42,6 @@ $initials = strtoupper(substr($CURRENT_USER_NAME, 0, 1));
       Dashboard
     </a>
     <?php if ($CURRENT_USER_ROLE !== 'admin'): ?>
-    <a class="nav-item" href="segment.php">
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 12h18M3 6h18M3 18h18"/></svg>
-      Road Setup
-    </a>
     <div class="nav-section">Reports</div>
     <a class="nav-item" href="road_result.php">
       <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"/></svg>
